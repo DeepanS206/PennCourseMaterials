6b40994abb8b917ae4169e44a0ac50803c24879d
Deepan Saravanan <deepan.saravanan83@gmail.com>
2017-01-21 02:13:01 -0500

    database setup finished

---
+++ models/app.js
@@ -0,0 +1,86 @@
+/*var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var drop = function(db, name, callback) {
+  db.dropCollection(name, function(err, results) {
+    callback(results);
+  });
+};
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      drop(db, 'documents', function() {
+        db.close();
+      });
+    });
+  });
+});*/
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+var insertUser = function(record, db, callback) {
+  var users = db.collection('users');
+  users.insertOne(record, function(err, r) {
+    if (err) {
+      console.log("sumting wong");
+      callback(err, null);
+    } else {
+      callback(false, r);
+    }
+  });
+};
+
+module.exports {
+  addUser: function(username, pw, fullname, email, callback) {
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+      doc = {'username':username, 'pw':pw, 'fullname':fullname, 'email':email};
+      insertUser(doc, db, function(err, r) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(r);
+        }
+      });
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser'); 
+var mongodb = require('./models/app.js'); 
 var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
+
+/*{ reg_username: 'Deepan',
+  reg_password: '',
+  reg_password_confirm: '',
+  reg_email: '',
+  reg_fullname: '' }*/
+
+app.get('/home', function(req, res) {
+  res.render('home.ejs');
+});
+
+app.post('/addUser', function(req, res) {
+  if (req.body.reg_password === req.body.reg_password_confirm) {
+    mongodb.addUser(req.body.reg_username, 
+    req.body.reg_password, req.body.reg_fullname, 
+    req.body.reg_email, function(err, r) {
+      if (!err) {
+        res.redirect('/home');
+      }
+    });
+  } else {
+    console.log('pw do not match');
+    res.redirect('/');
+  }
+});
 
 app.get('/', function(req, res) {
   res.render('login.ejs');
