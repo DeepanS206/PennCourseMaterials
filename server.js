1df12c62a535b85795dd8602c0e0d9d74b132bd3
Deepan Saravanan <deepan.saravanan83@gmail.com>
2017-01-20 22:43:41 -0500

    set up basic
    login page with server functionality

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-//comment
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('login.ejs');
+});
+
+console.log('Author: Deepan Saravanan (deepans)');
+http.createServer(app).listen(process.env.PORT || 8080)
+console.log('Server running on port 8080. Now open http://localhost:8080/ in your browser!');
